98240dfaf3097f3772ec45cdf2723fdbdba52982
Jon Wolff <jon.wolff@wolfftech.com>
2023-12-30 00:57:24 +0000

    prisma start

---
+++ prisma/migrations/20231230003702_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `Brands` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `brandName` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Models` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `modelNumber` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello Typescript')
+import * as mysql from 'mysql2';
+
+export class Server {
+    public readonly connection: mysql.Connection;
+    
+connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'scrapper',
+    password: 'Scrapper#1322',
+    database: 'scrapper'
+});
+
+this.connection.connect(function(err: any) {
+    if(err)
+    {
+        return console.error('error: ' + err.message);
+    }
+    
+    console.log('Connected to the MySQL Server.');
+});
